cc740f7cc7e8e2a168e1bd4665fa00f4b930ad5e
juliacalvert <147757218+juliacalvert@users.noreply.github.com>
2025-05-12 16:01:55 -0400

    carousel

--- project.js
+++ project.js
@@ -1,36 +1,3 @@
-/*function getImages() {
-    const url = "https://api.fbi.gov/wanted/v1/list";
-    
-    fetch(url)
-        .then((result) => result.json())
-        .then(data => {
-            const carousel = document.getElementById('carousel');
-            const criminals = data.items.slice(0, 10);
-
-            criminals.forEach((criminal) => {
-                if (criminal.images && criminal.images[0]) {
-                    const img = document.createElement('img');
-                    img.src = criminal.images[0].original;
-                    img.alt = criminal.title || "Most Wanted Criminal";
-                    carousel.appendChild(img);
-                    console.log('Criminals fetched:', criminals);
-                    console.log('Images added:', carousel.children.length);
-                }
-            });
-            simpleslider.getSlider({
-                container: document.getElementById('carousel'),
-                delay: 3,
-                duration: 0.5
-            });
-        })
-
-}
-
-
-window.onload = getImages();*/
-
-
-
 function getImages() {
     const url = "https://api.fbi.gov/wanted/v1/list";
     
@@ -49,7 +16,6 @@ function getImages() {
                 }
             });
 
-            // Give DOM a chance to reflow before initializing slider
             setTimeout(() => {
                 simpleslider.getSlider({
                     container: carousel,
